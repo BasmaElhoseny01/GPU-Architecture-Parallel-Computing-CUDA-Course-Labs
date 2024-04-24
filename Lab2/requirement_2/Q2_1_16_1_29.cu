e7e93f6890c07a270b4dd8f02b57024bd625b144
Sarah Elzayat <sarahelzayat@outlook.com>
2024-04-24 12:06:44 +0200

    reqs fixed

--- Lab2/requirement_2/Q2_1_16_1_29.cu
+++ Lab2/requirement_2/Q2_1_16_1_29.cu
@@ -20,6 +20,46 @@ __global__ void vector_add(float *out, float *matrix, float *vector, int N, int 
     }
 }
 
+void read_matrices(FILE *file, const int rows, const int cols, float *a, float *b)
+{
+    //[row,col] --> flattening col+row*width
+    // 2. Read values row by row [Matrix 1]
+    for (int i = 0; i < rows; i++)
+    {
+        // 3. Read col by col
+        for (int j = 0; j < cols; j++)
+        {
+            float num;
+            if (fscanf(file, "%f", &num) != 1)
+            {
+                printf("Invalid input. Please enter a float num. \n");
+                exit(1);
+            }
+            // printf("%f\n",num);
+            a[j + i * cols] = num;
+        }
+        // skip_comments(file);
+    }
+
+    // 2. Read values row by row [Matrix 2]
+    for (int i = 0; i < rows; i++)
+    {
+        // 3. Read col by col
+        for (int j = 0; j < cols; j++)
+        {
+            float num;
+            if (fscanf(file, "%f", &num) != 1)
+            {
+                printf("Invalid input. Please enter a float num. \n");
+                exit(1);
+            }
+            // printf("%f\n",num);
+            b[j + i * cols] = num;
+        }
+        // skip_comments(file);
+    }
+}
+
 int main(int argc, char *argv[])
 {
     char *input_path = argv[1];
@@ -33,26 +73,47 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-    char line[5000];
+    // Reading file content
+    int test_cases;
+    if (fscanf(file, "%d", &test_cases) == EOF)
+    {
+        printf("Error in reading int no of testcases in the first line");
+        exit(1);
+    }
+    printf("%d\n", test_cases);
+    // char line[5000];
 
-    int test_cases = atoi(fgets(line, sizeof(line), file));
-    printf("test_cases: %d\n", test_cases);
+    // int test_cases = atoi(fgets(line, sizeof(line), file));
+    // printf("test_cases: %d\n", test_cases);
 
     for (int k = 0; k < test_cases; k++)
     {
-        int matrix_rows, matrix_columns;
-        const char delimiter[] = " ";
-        char *matrix_line = fgets(line, sizeof(line), file);
+        // int matrix_rows, matrix_columns;
+        // const char delimiter[] = " ";
+        // char *matrix_line = fgets(line, sizeof(line), file);
 
-        matrix_rows = atoi(strtok(matrix_line, delimiter));
-        matrix_columns = atoi(strtok(NULL, delimiter));
-        printf("\nmatrix_rows: %d\n", matrix_rows);
-        printf("matrix_columns: %d\n", matrix_columns);
+        // matrix_rows = atoi(strtok(matrix_line, delimiter));
+        // matrix_columns = atoi(strtok(NULL, delimiter));
+        // printf("\nmatrix_rows: %d\n", matrix_rows);
+        // printf("matrix_columns: %d\n", matrix_columns);
+
+        // 1. Read no of rows and cols
+        int matrix_rows, matrix_columns;
+
+        if (fscanf(file, "%d %d", &matrix_rows, &matrix_columns) != 2)
+        {
+            printf("Invalid input. Please enter two integers. to represnt rows and cols respectively \n");
+            exit(1);
+        }
+        printf("%d-%d\n", matrix_rows, matrix_columns);
 
         // Allocate host memory
         float *matrix = (float *)malloc(sizeof(float) * matrix_rows * matrix_columns);
         float *vector = (float *)malloc(sizeof(float) * matrix_columns);
         float *out = (float *)malloc(sizeof(float) * matrix_rows);
+
+        // Read matrices
+        // read_matrices(file, matrix_rows, matrix_columns, matrix, vector);
 
         // initialize out with zeros
         for (int i = 0; i < matrix_rows; i++)
@@ -61,36 +122,43 @@ int main(int argc, char *argv[])
         }
         for (int i = 0; i < matrix_rows; i++)
         {
-            char *matrix_line = fgets(line, sizeof(line), file);
-            char *token = strtok(matrix_line, delimiter);
             for (int j = 0; j < matrix_columns; j++)
             {
-                printf("%d", i * matrix_columns + j);
-                matrix[(i * matrix_columns) + j] = atof(token);
-                token = strtok(NULL, delimiter);
+                float num;
+                if (fscanf(file, "%f", &num) != 1)
+                {
+                    printf("Invalid input. Please enter a float num. \n");
+                    exit(1);
+                }
+                matrix[j + i * matrix_columns] = num;
             }
         }
 
         // each vector element is in a new line
         for (int i = 0; i < matrix_columns; i++)
         {
-            char *vector_line = fgets(line, sizeof(line), file);
-            vector[i] = atof(vector_line);
+            float num;
+            if (fscanf(file, "%f", &num) != 1)
+            {
+                printf("Invalid input. Please enter a float num. \n");
+                exit(1);
+            }
+            vector[i] = num;
         }
 
-        printf("\nMatrix\n");
-        // print the matrix and the vector
-        for (int i = 0; i < matrix_rows * matrix_columns; i++)
-        {
-            printf("%f ", matrix[i]);
-        }
+        // // printf("\nMatrix\n");
+        // // print the matrix and the vector
+        // for (int i = 0; i < matrix_rows * matrix_columns; i++)
+        // {
+        //     printf("%f ", matrix[i]);
+        // }
 
-        printf("\nVector\n");
-        for (int i = 0; i < matrix_columns; i++)
-        {
-            printf("%f ", vector[i]);
-        }
-        printf("\n");
+        // // printf("\nVector\n");
+        // for (int i = 0; i < matrix_columns; i++)
+        // {
+        //     printf("%f ", vector[i]);
+        // }
+        // printf("\n");
 
         float *d_matrix, *d_vector, *d_out;
 
@@ -110,10 +178,10 @@ int main(int argc, char *argv[])
         // Transfer data back to host memory
         cudaMemcpy(out, d_out, sizeof(float) * matrix_rows, cudaMemcpyDeviceToHost);
 
-        for (int i = 0; i < matrix_rows; i++)
-        {
-            printf("\nout[%d] = %f", i, out[i]);
-        }
+        // for (int i = 0; i < matrix_rows; i++)
+        // {
+        //     printf("\nout[%d] = %f", i, out[i]);
+        // }
 
         // Verification :D
         for (int i = 0; i < matrix_rows; i++)
